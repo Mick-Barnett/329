ca30828838c96764ad0c2ee23a0ce352a2496e15
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-06 14:56:44 -0700

    Create LPUART1_PART1.h

---
+++ LPUART1_PART1.h
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file           : LPUART1.h
+  * @brief          : Header for LPUART1.c file.
+  * project         : EE 329 S'26 A7
+  * authors         : Joseph Matella & Mick Barnett
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#include "stm32l4xx_hal.h"
+#include <stdio.h>
+
+typedef int bool;
+
+// Function declarations
+void LPUART1_init(void);
+void LPUART_Print(const char* message);
+void LPUART1_IRQHandler(void);
+void LPUART_ESC_Print(const char* command);
+
