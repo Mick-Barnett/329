cc84c6e30b910305b426cfa225ff30a7a137de8f
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-06 15:03:31 -0700

    Add game display constants and function declarations

--- LPUART1.h
+++ LPUART1.h
@@ -18,13 +18,30 @@
   */
 
 #include "stm32l4xx_hal.h"
+#include "delay.h"
 #include <stdio.h>
 
 typedef int bool;
+
+// Rows and columns of splash screen
+#define ROWS 24
+#define COLS 80
+
+// Game border defines to account for offset from edge
+#define BORDER_OFFSET 4
+#define BORDER_ROW_MAX (ROWS - BORDER_OFFSET)
+#define BORDER_ROW_MIN (BORDER_OFFSET + 1)
+#define BORDER_COL_MAX (COLS - BORDER_OFFSET)
+#define BORDER_COL_MIN (BORDER_OFFSET + 1)
 
 // Function declarations
 void LPUART1_init(void);
 void LPUART_Print(const char* message);
 void LPUART1_IRQHandler(void);
 void LPUART_ESC_Print(const char* command);
+void LPUART_ESC_Print_Pos(uint8_t row, uint8_t col);
+void Render_SplashScreen(void);
+void Render_Border(void);
+void Update_Character_Position(void);
+void Render_Character(void);
 
