a82b8cbd91ba7dc42ad7e914497456762e1c342b
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-06 14:41:55 -0700

    Create main.h for common defines and prototypes
    Added header file for main.c with common defines and function prototypes.

---
+++ main.h
@@ -0,0 +1,43 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  * project         : EE 329 S'26 A7
+  * authors         : Joseph Matella & Mick Barnett
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+// Typedef
+typedef int bool;
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
