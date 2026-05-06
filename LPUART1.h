6df90bf7130f5f672329cc80f559174f6fc53632
joeymatella2 <111457339+joeymatella2@users.noreply.github.com>
2026-05-06 15:02:58 -0700

    Revise header documentation in LPUART1.h
    Updated header comments and authors in LPUART1.h

--- LPUART1.h
+++ LPUART1.h
@@ -1,38 +1,30 @@
-/*
- *******************************************************************************
- * @file           : LPUART1.h
- * @brief          : X
- * project         : EE 329 S'26 AX
- * authors         : joeym
- * version         : 0.1
- * date            : May 4, 2026
- * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
- * target          : NUCLEO-L4A6ZG
- * clocks          : 4 MHz MSI to AHB2
- * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
- *******************************************************************************
- * Description: X
- *
- *******************************************************************************
- * GPIO Wiring
- * |   Component    | GPIO Identifier | Connector Location | Config
- *-----------------------------------------------------------------------------
- * | LCD - DB4 - 11 | PC0             | CN9-3              | OUT
- *******************************************************************************
- * Version History
- *  Ver.|   Date   |  Description
- *  ---------------------------------------------------------------------------
- *      |          | 
- *******************************************************************************
- *
- * Header format adapted from [Code Appendix by Kevin Vo] pg 5
- */
+/**
+  ******************************************************************************
+  * @file           : LPUART1.h
+  * @brief          : Header for LPUART1.c file.
+  * project         : EE 329 S'26 A7
+  * authors         : Joseph Matella & Mick Barnett
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
 #include "stm32l4xx_hal.h"
 #include <stdio.h>
 
+typedef int bool;
+
+// Function declarations
 void LPUART1_init(void);
 void LPUART_Print(const char* message);
 void LPUART1_IRQHandler(void);
 void LPUART_ESC_Print(const char* command);
-
 
