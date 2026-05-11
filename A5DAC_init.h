e227d069a4e7acee41ca79c6eaa0a267cd51f632
Mick-Barnett <156118755+Mick-Barnett@users.noreply.github.com>
2026-05-11 16:21:10 -0700

    Create A5DAC_init.h

---
+++ A5DAC_init.h
@@ -0,0 +1,15 @@
+/*
+ * DAC_init.h
+ *
+ *  Created on: May 11, 2026
+ *      Author: mickp
+ */
+
+#ifndef DAC_INIT_H_
+#define DAC_INIT_H_
+
+#include "stm32l4xx.h"   // register definitions
+
+void DAC_init(void);
+
+#endif /* DAC_INIT_H_ */
