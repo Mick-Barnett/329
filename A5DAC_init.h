5dd392547cc206beeb484df1b0b3ede8664b75f8
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2026-05-11 16:38:50 -0700

    Update A5DAC_init.h

--- A5DAC_init.h
+++ A5DAC_init.h
@@ -1,15 +1,40 @@
+#ifndef DAC_INIT_H
+#define DAC_INIT_H
+
+#include "stm32l4xx.h"
+#include <stdint.h>
+
 /*
- * DAC_init.h
+ * ============================================================
+ * DAC MODULE (MCP4821)
  *
- *  Created on: May 11, 2026
- *      Author: mickp
+ * Pin Mapping:
+ * PA5  -> SPI1_SCK  -> DAC SCK
+ * PA7  -> SPI1_MOSI -> DAC SDI
+ * PD14 -> GPIO      -> DAC CS (MANUAL CHIP SELECT)
+ *
+ * IMPORTANT:
+ * PD14 is NOT SPI alternate function.
+ * It is controlled manually in DAC_write().
+ *
+ * MCP4821 connections:
+ * VDD  -> 3.3V
+ * VSS  -> GND
+ * LDAC -> GND
+ * SHDN -> 3.3V
+ * VOUT -> analog output
+ * ============================================================
  */
 
-#ifndef DAC_INIT_H_
-#define DAC_INIT_H_
+#define DAC_MAX_MV      3300U
+#define DAC_MAX_CODE    4095U
 
-#include "stm32l4xx.h"   // register definitions
+/* MCP4821 control bits (0x1000 = active + gain=2) */
+#define DAC_CTRL_BITS   0x1000U
 
 void DAC_init(void);
+uint16_t DAC_volt_conv(uint16_t millivolts);
+void DAC_write(uint16_t dac_code);
+void DAC_set_mV(uint16_t millivolts);
 
-#endif /* DAC_INIT_H_ */
+#endif
