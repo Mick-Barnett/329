68ddfd244480c8b9675009ae62a3082715b5b864
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2026-05-11 16:58:42 -0700

    Update A5DAC_write.h

--- A5DAC_write.h
+++ A5DAC_write.h
@@ -1,1 +1,21 @@
+#ifndef DAC_WRITE_H
+#define DAC_WRITE_H
 
+#include "stm32l4xx.h"
+#include <stdint.h>
+
+/*
+ * Writes one 12-bit DAC data value to the MCP4821 over SPI1.
+ *
+ * Pin mapping:
+ * PA5  -> SPI1_SCK
+ * PA7  -> SPI1_MOSI / SDI
+ * PD14 -> DAC CS
+ *
+ * Assumes:
+ * - SPI1 has already been initialized
+ * - PD14 has already been configured as GPIO output
+ */
+void DAC_write(uint16_t dac_data);
+
+#endif
