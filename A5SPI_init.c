59a333c544f6b385405577d6aaa98f7732cc47e6
Mick-Barnett <156118755+Mick-Barnett@users.noreply.github.com>
2026-05-11 17:18:26 -0700

    Update A5SPI_init.c

--- A5SPI_init.c
+++ A5SPI_init.c
@@ -14,7 +14,7 @@ void SPI_init( void ) {
    // CR1 (reset value = 0x0000)
    SPI1->CR1 &= ~( SPI_CR1_SPE );             	// disable SPI for config
    SPI1->CR1 &= ~( SPI_CR1_RXONLY );          	// recv-only OFF
-   SPI1->CR1 &= ~( SPI_CR1_MSBFIRST );        	// data bit order MSb:LSb
+   SPI1->CR1 &= ~( SPI_CR1_LSBFIRST );        	// data bit order MSb:LSb
    SPI1->CR1 &= ~( SPI_CR1_CPOL | SPI_CR1_CPHA ); // SCLK polarity:phase = 0:0
    SPI1->CR1 |=	 SPI_CR1_MSTR;              	// MCU is SPI controller
    // CR2 (reset value = 0x0700 : 8b data)
