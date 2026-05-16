3e92e49fcd4922fe24fa68390bf26736694f108a
Fac943 <149449123+Fac943@users.noreply.github.com>
2026-05-15 17:42:33 -0700

    SPI.c functions

--- A9_I2C.c
+++ A9_I2C.c
@@ -1,6 +1,6 @@
 /*
  *******************************************************************************
- * EE 329 A9 I2C & EEPROM
+ * EE 329 A5 NUCLEO SPI & DAC INTERFACE
  *******************************************************************************
  * @file           : I2C.c
  * @brief          : Inits I2C1 and contains read/write functions
@@ -17,4 +17,52 @@
  *******************************************************************************
  * Header format adapted from [Code Appendix by Kevin Vo] pg 5
 
+ /* USER configure GPIO pins for I2C alternate functions SCL and SDA */
+// Configure I2C
+void SPI_Init(void) {
+	RCC->APB1ENR1 |= RCC_APB1ENR1_I2C1EN; // enable I2C bus clock
+	I2C1->CR1 &= ~(I2C_CR1_PE); // put I2C into reset (release SDA, SCL)
+	I2C1->CR1 &= ~(I2C_CR1_ANFOFF); // filters: enable analog
+	I2C1->CR1 &= ~(I2C_CR1_DNF); // filters: disable digital
+	I2C1->TIMINGR = 0x00303D5B; // 16 MHz SYSCLK timing from CubeMX
+	I2C1->CR2 |= (I2C_CR2_AUTOEND); // auto send STOP after transmission
+	I2C1->CR2 &= ~(I2C_CR2_ADD10); // 7-bit address mode
+	I2C1->CR1 |= (I2C_CR1_PE); // enable I2C
+}
+
+void SPI_Write(void) {
+// build EEPROM transaction
+	I2C1->CR2 &= ~(I2C_CR2_RD_WRN); // set WRITE mode
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES); // clear Byte count
+	I2C1->CR2 |= (3 << I2C_CR2_NBYTES_Pos); // write 3 bytes (2 addr, 1 data)
+	I2C1->CR2 &= ~(I2C_CR2_SADD); // clear device address
+	I2C1->CR2 |= (EEPROM_ADDRESS << (I2C_CR2_SADD_Pos + 1)); // device addr SHL 1
+	I2C1->CR2 |= I2C_CR2_START; // start I2C WRITE op
+	/* USER wait for I2C_ISR_TXIS to clear before writing each Byte, e.g. ... */
+	while (!(I2C1->ISR & I2C_ISR_TXIS))
+		;  // wait for start condition to transmit
+	I2C1->TXDR = (EEPROM_MEMORY_ADDR >> 8);
+	// xmit MSByte of address
+	/* address high, address low, data  -  wait at least 5 ms before READ
+	 the READ op has new NBYTES (WRITE 2 then READ 1) & new RD_WRN for 3rd Byte */
+	I2C1->TXDR = (data); //send data
+}
+
+void SPI_Read(void) {
+	// build EEPROM transaction
+	I2C1->CR2 &= ~(I2C_CR2_RD_WRN); // set WRITE mode
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES); // clear Byte count
+	I2C1->CR2 |= (3 << I2C_CR2_NBYTES_Pos); // write 3 bytes (2 addr, 1 data)
+	I2C1->CR2 &= ~(I2C_CR2_SADD); // clear device address
+	I2C1->CR2 |= (EEPROM_ADDRESS << (I2C_CR2_SADD_Pos + 1)); // device addr SHL 1
+	I2C1->CR2 |= I2C_CR2_START; // start I2C WRITE op
+	/* USER wait for I2C_ISR_TXIS to clear before writing each Byte, e.g. ... */
+	while (!(I2C1->ISR & I2C_ISR_TXIS))
+		;  // wait for start condition to transmit
+	I2C1->TXDR = (EEPROM_MEMORY_ADDR >> 8);
+	// xmit MSByte of address
+	/* address high, address low, data  -  wait at least 5 ms before READ
+	 the READ op has new NBYTES (WRITE 2 then READ 1) & new RD_WRN for 3rd Byte */
+	I2C1->TXDR = (data); //send data
+}
 
