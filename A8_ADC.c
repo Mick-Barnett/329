f96be5cf3bb57ab48dacd45bdf5b692689d0b3de
Mick-Barnett <156118755+Mick-Barnett@users.noreply.github.com>
2026-05-28 10:45:21 -0700

    Update A8_ADC.c

--- A8_ADC.c
+++ A8_ADC.c
@@ -1,8 +1,23 @@
 /*
- * ADC.c
- *
- *  Created on: May 26, 2026
- *      Author: mickp
+ *******************************************************************************
+ * EE 329 A9 I2C & EEPROM
+ *******************************************************************************
+ * @file           : ADC.c
+ * @brief          : Src file for ADC.c code
+ * project         : EE 329 S'26 A8
+ * authors         : Mick Barnett, Tyler Ragasa, Alan Odnoblyudov
+ * version         : 0.3
+ * date            : 05/26/2026
+ * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 24 MHz MSI to AHB2
+ * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * PIN ASSIGNMENTS
+ *PC0 --> GPIO Out
+ *PA0 --> Analog Input
+ *******************************************************************************
+ * Header format adapted from [Code Appendix by Kevin Vo] pg 5
  */
 #include "ADC.h"
 
@@ -75,29 +90,37 @@ void ADC1_2_IRQHandler(void) {
 	}
 }
 
+//converts to mV by subtracting known offset and dividing by conversion rate defined in ADC.h
 uint16_t ADC_count_to_mV(uint16_t count) {
 	if(count<=CAL_OFFSET) return 0;
 	uint32_t mV = (((uint32_t)count - CAL_OFFSET)*CAL_SLOPE_DEN)/CAL_SLOPE_NUM;
 	return (uint16_t)mV;
 }
 
-void Process_ADC_samples(uint16_t array[], uint8_t length) {
-	uint32_t sum = 0;
 
+void Process_ADC_samples(uint16_t array[], uint8_t length) {
+//intializes sum at 0	
+	uint32_t sum = 0;
+//intializes adc min and max as the 0 index to start
 	adc_min_count = array[0];
 	adc_max_count = array[0];
 
+	//increments through the indexs with each sample to find min/max
 	for (uint8_t i = 0; i < length; i++) {
-		// Parse through
+		//compares each sample to known min, if sample is lower, it becomes the new min
 		if (array[i] < adc_min_count) {
 			adc_min_count = array[i];
 		}
+		//compares each sample to known max, if sample is higher, it becomes the new max
 		if (array[i] > adc_max_count) {
 			adc_max_count = array[i];
 		}
+		//adds each new sample to the sum
 		sum += array[i];
 	}
+	//takes average
 	adc_avg_count = (uint16_t)(sum / length);
+	//converts to mV
 	adc_min_mV = ADC_count_to_mV(adc_min_count);
 	adc_max_mV = ADC_count_to_mV(adc_max_count);
 	adc_avg_mV = ADC_count_to_mV(adc_avg_count);
